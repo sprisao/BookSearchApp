44ff9894462f34c574c8e7c7817f780dc85400d1
devbrx <brucechoe@sejonggroup.kr>
2022-12-09 17:38:00 +0900

    Add RetrofitInstance.kt

---
+++ app/src/main/java/com/example/booksearchapp/data/model/api/RetrofitInstance.kt
@@ -0,0 +1,22 @@
+package com.example.booksearchapp.data.model.api
+
+import com.example.booksearchapp.util.Constants.BASE_URL
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+object RetrofitInstance {
+    private val okHttpClient: OkHttpClient by lazy {
+        val httpLoggingInterceptor = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+        OkHttpClient.Builder().addInterceptor(httpLoggingInterceptor).build()
+    }
+    private val retrofit: Retrofit by lazy {
+        Retrofit.Builder().addConverterFactory(MoshiConverterFactory.create()).baseUrl(BASE_URL)
+            .client(okHttpClient).build()
+    }
+
+    val api: BookSearchApi by lazy {
+        retrofit.create(BookSearchApi::class.java)
+    }
+}

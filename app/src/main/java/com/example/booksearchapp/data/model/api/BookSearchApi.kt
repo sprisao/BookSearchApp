379bf1b8bec376efdec2aae73040f5c24acac0f5
devbrx <brucechoe@sejonggroup.kr>
2022-12-09 17:28:20 +0900

    Add BookSearchApi class

---
+++ app/src/main/java/com/example/booksearchapp/data/model/api/BookSearchApi.kt
@@ -0,0 +1,22 @@
+package com.example.booksearchapp.data.model.api
+
+import com.example.booksearchapp.data.model.SearchResponse
+import com.example.booksearchapp.util.Constants
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Query
+
+interface BookSearchApi {
+
+    @Headers("Authorization: KakaoAK ${Constants.API_KEY}")
+    @GET("/v3/search/book")
+    suspend fun searchBooks(
+        @Query("query") query: String,
+        @Query("sort") sort: String,
+        @Query("page") page: Int,
+        @Query("size") size: Int
+    ): Response<SearchResponse>
+
+
+}

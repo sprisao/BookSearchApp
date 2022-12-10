e218f1b0302fbd6c6e165b25fc3712624db04238
devbrx <brucechoe@sejonggroup.kr>
2022-12-10 17:35:09 +0900

    Add ViewModelProviderFactory

---
+++ app/src/main/java/com/example/booksearchapp/ui/viewmodel/BookSearchViewModelProviderFactory.kt
@@ -0,0 +1,18 @@
+package com.example.booksearchapp.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.booksearchapp.data.repository.BookSearchRepository
+
+@Suppress("UNCHECKED_CAST")
+class BookSearchViewModelProviderFactory(
+    private val bookSearchRepository: BookSearchRepository
+) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(BookSearchViewModel::class.java)) {
+            return BookSearchViewModel(bookSearchRepository) as T
+        }
+        throw IllegalArgumentException("ViewModel class not found")
+    }
+}
+

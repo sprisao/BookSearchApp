e397628518deb1b8f3bc4be2746bb8788a4d4b3c
devbrx <brucechoe@sejonggroup.kr>
2022-12-10 12:07:33 +0900

    Add ViewModel

---
+++ app/src/main/java/com/example/booksearchapp/ui/viewmodel/BookSearchViewModel.kt
@@ -0,0 +1,27 @@
+package com.example.booksearchapp.ui.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.booksearchapp.data.model.SearchResponse
+import com.example.booksearchapp.data.repository.BookSearchRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class BookSearchViewModel(private val bookSearchRepository: BookSearchRepository) : ViewModel() {
+
+    private val _searchResult = MutableLiveData<SearchResponse>()
+    val searchResult: LiveData<SearchResponse> get() = _searchResult
+
+    fun searchBooks(query: String) = viewModelScope.launch(Dispatchers.IO) {
+        val response = bookSearchRepository.searchBooks(query, "accuracy", 1, 50)
+        if (response.isSuccessful) {
+            response.body()?.let { body ->
+                _searchResult.postValue(body)
+            }
+        }
+    }
+
+}
+

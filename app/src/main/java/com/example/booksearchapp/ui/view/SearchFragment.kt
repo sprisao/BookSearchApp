5514da291ee0caafd3660a78728f992593b65e32
devbrx <brucechoe@sejonggroup.kr>
2022-12-11 17:53:30 +0900

    [검색결과를 UI에 표시하기] : Add ViewModel to SearchFragment

--- app/src/main/java/com/example/booksearchapp/ui/view/SearchFragment.kt
+++ app/src/main/java/com/example/booksearchapp/ui/view/SearchFragment.kt
@@ -5,24 +5,42 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.booksearchapp.MainActivity
 import com.example.booksearchapp.databinding.FragmentSearchBinding
+import com.example.booksearchapp.ui.adapter.BookSearchAdapter
+import com.example.booksearchapp.ui.viewmodel.BookSearchViewModel
 
 
 class SearchFragment : Fragment() {
     private var _binding: FragmentSearchBinding? = null
-
-    /* binding을 get으로 한번 더 불러온다 하지만 왜? */
     private val binding get() = _binding!!
 
+    private lateinit var bookSearchViewModel: BookSearchViewModel
+    private lateinit var bookSearchAdapter: BookSearchAdapter
+
     override fun onCreateView(
-
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
-
         /* 여기서 nullable 하게 처리한다 View가 없을 경우도 있으니*/
-
     ): View? {
         _binding = FragmentSearchBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        bookSearchViewModel = (activity as MainActivity).bookSearchViewModel
+    }
+
+    private fun setupRecyclerView() {
+        bookSearchAdapter = BookSearchAdapter()
+        binding.rvSearchResult.apply {
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+            addItemDecoration(DividerItemDecoration(requireContext(), LinearLayoutManager.VERTICAL))
+            adapter = bookSearchAdapter
+        }
     }
 
     override fun onDestroyView() {

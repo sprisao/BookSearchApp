3d20c661f8cda69fc64623dbfe0e815a105ce534
devbrx <brucechoe@sejonggroup.kr>
2022-12-14 09:18:07 +0900

    [Room DB <-> UI 연동] Fix build error : room-dependency 버전 업그레이드, BookSearchDatabase 코드 수정, (추가: bottom_navigation_menu.xml 네비게이션 메뉴 순서 변경)

--- app/src/main/java/com/example/booksearchapp/data/db/BookSearchDatabase.kt
+++ app/src/main/java/com/example/booksearchapp/data/db/BookSearchDatabase.kt
@@ -2,32 +2,36 @@ package com.example.booksearchapp.data.db
 
 import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.example.booksearchapp.data.model.Book
+
 
 @Database(
     entities = [Book::class],
     version = 1,
     exportSchema = false
 )
-
 @TypeConverters(OrmConverter::class)
 abstract class BookSearchDatabase : RoomDatabase() {
+
     abstract fun bookSearchDao(): BookSearchDao
 
     companion object {
         @Volatile
         private var INSTANCE: BookSearchDatabase? = null
 
-        private fun buildDatabase(context: Context): BookSearchDatabase = androidx.room.Room.databaseBuilder(
-            context.applicationContext,
-            BookSearchDatabase::class.java,
-            "book_search_database"
-        ).build()
+        private fun buildDatabase(context: Context): BookSearchDatabase =
+            Room.databaseBuilder(
+                context.applicationContext,
+                BookSearchDatabase::class.java,
+                "favorite-books"
+            ).build()
 
-        fun getInstance(context: Context): BookSearchDatabase = INSTANCE ?: synchronized(this) {
-            INSTANCE ?: buildDatabase(context).also { INSTANCE = it }
-        }
+        fun getInstance(context: Context): BookSearchDatabase =
+            INSTANCE ?: synchronized(this) {
+                INSTANCE ?: buildDatabase(context).also { INSTANCE = it }
+            }
     }
 }

27df4dc48e91d1c66bff26f0af61ad3f6e75c93d
devbrx <brucechoe@sejonggroup.kr>
2022-12-14 09:40:54 +0900

    [Room DB <-> UI 연동] Favorite 버튼으로 DB에 저장된 favoriteBooks를 FavoriteFragment에서 RecyclerView를 통해 보여줌 + 왼쪽으로 스와이프 시 DB에서 제거되는 기능 구현

--- app/src/main/java/com/example/booksearchapp/ui/view/FavoriteFragment.kt
+++ app/src/main/java/com/example/booksearchapp/ui/view/FavoriteFragment.kt
@@ -5,17 +5,90 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.booksearchapp.databinding.FragmentFavoriteBinding
+import com.example.booksearchapp.ui.adapter.BookSearchAdapter
+import com.example.booksearchapp.ui.viewmodel.BookSearchViewModel
+import com.google.android.material.snackbar.Snackbar
 
 class FavoriteFragment : Fragment() {
     private var _binding: FragmentFavoriteBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var bookSearchViewModel: BookSearchViewModel
+    private lateinit var bookSearchAdapter: BookSearchAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentFavoriteBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        bookSearchViewModel = (activity as MainActivity).bookSearchViewModel
+
+        setupRecyclerView()
+        setupTouchHelper(view)
+
+        bookSearchViewModel.favoriteBooks.observe(viewLifecycleOwner) {
+            bookSearchAdapter.submitList(it)
+        }
+
+    }
+
+    private fun setupRecyclerView() {
+        bookSearchAdapter = BookSearchAdapter()
+        binding.rvFavoriteBooks.apply {
+            setHasFixedSize(true)
+            layoutManager =
+                LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+            addItemDecoration(
+                DividerItemDecoration(
+                    requireContext(),
+                    DividerItemDecoration.VERTICAL
+                )
+            )
+            adapter = bookSearchAdapter
+        }
+        bookSearchAdapter.setOnItemClickListener {
+            findNavController().navigate(
+                SearchFragmentDirections.actionFragmentSearchToFragmentBook(it)
+            )
+        }
+    }
+
+    private fun setupTouchHelper(view: View) {
+        val itemTouchHelperCallback = object : ItemTouchHelper.SimpleCallback(
+            0, ItemTouchHelper.LEFT
+        ) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.bindingAdapterPosition
+                val book = bookSearchAdapter.currentList[position]
+                bookSearchViewModel.deleteBook(book)
+                Snackbar.make(view, "Successfully deleted book", Snackbar.LENGTH_LONG).apply {
+                    setAction("Undo") {
+                        bookSearchViewModel.saveBook(book)
+                    }
+                }.show()
+            }
+        }
+        ItemTouchHelper(itemTouchHelperCallback).apply {
+            attachToRecyclerView(binding.rvFavoriteBooks)
+        }
     }
 
     override fun onDestroyView() {
